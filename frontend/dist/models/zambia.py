7736125f6dd0d1280473d8dc810d2b44559f2a71
Chris Arderne <chris@rdrn.me>
2024-07-15 08:50:42 +0100

    add dummy calculation

--- frontend/dist/models/zambia.py
+++ frontend/dist/models/zambia.py
@@ -1,17 +1,23 @@
 from typing import TypedDict
 
 
-class Town(TypedDict): ...
+class Town(TypedDict):
+    hh: int
+    hh_access: int
 
 
-class Pars(TypedDict): ...
+class Pars(TypedDict):
+    interest_rate: float
+    num_years: int
 
 
 class Result(TypedDict):
-    profit: int
+    profit: float
 
 
 def model(town: Town, pars: Pars) -> Result:
+    # made-up calculation
+    profit = town["hh"] * pars["interest_rate"] + town["hh_access"] * pars["num_years"]
     return Result(
-        profit=0,
+        profit=profit,
     )
